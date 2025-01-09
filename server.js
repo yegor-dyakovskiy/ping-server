65e35709870c249edf8a3898ca47dc92b1a53939
Yagor <yegor.dyakovskiy@gmail.com>
2025-01-09 10:44:39 +0500

    add console log

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
+import http from 'http';
 import https from 'https';
 
-// URL для пинга
 const URL = 'https://promalp-lead-server.onrender.com';
 
-// Функция для отправки запроса
 const pingServer = () => {
     https.get(URL, (res) => {
         console.log(`Пинг отправлен на ${URL}. Статус ответа: ${res.statusCode}`);
@@ -13,6 +12,17 @@ const pingServer = () => {
 };
 
 // Отправлять запрос каждую минуту
-setInterval(pingServer, 60000); // 60000 миллисекунд = 1 минута
+setInterval(pingServer, 60000);
 
 console.log('Сервер для пинга запущен. Пингую каждый 1 минуту...');
+
+// Фиктивный сервер
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is a dummy server for Render compatibility.');
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+    console.log(`Фиктивный сервер запущен на порту ${PORT}`);
+});
